ac8f07399defd7943460a739a6a7398140fb844c
Michael Darko <mykdarkoxyz@gmail.com>
2025-05-28 23:45:35 +0000

    changed variable names

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,21 +1,21 @@
-first_number = int(input("Enter the first number: "))
-second_number = int(input("Enter the second number: "))
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
 operations = input("Choose the operation (+, -, *, /): ")
 
 match operations:
     case "+":
-        result = first_number + second_number
+        result = num1 + num2
         print(f"The result is {result}. ")
     case "-":
-        result = first_number - second_number
+        result = num1 - num2
         print(f"The result is {result}. ")
     case "*":
-        result = first_number * second_number
+        result = num1 * num2
         print(f"The result is {result}. ")
     case "/":
-        if second_number == 0:
+        if num2 == 0:
             print("Cannot divide by zero")
         else:
-            result = first_number / second_number
+            result = num1 / num2
             print(f"The result is {result}. ")
     
