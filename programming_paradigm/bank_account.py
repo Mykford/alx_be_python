0efd7579a63523f273906be5b7dc2b81fc4b0c5f
Michael Darko <mykdarkoxyz@gmail.com>
2025-06-11 08:20:56 +0000

    tried to fix error

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,5 +1,5 @@
 class BankAccount:
-    def __init__(self,account_balance=0):
+    def __init__(self,account_balance):
         self.account_balance = account_balance
         
     def deposit(self, amount):
@@ -9,12 +9,11 @@ class BankAccount:
     def withdraw(self, amount):
         if self.account_balance >= amount:
             self.account_balance -= amount
-            return self.account_balance
+            return True
         else:
             return False
     def display_balance(self):
-        print(f"Current Balance: {self.account_balance}")    
-        return self.account_balance
-      
+        print(f"Current Balance: ${self.account_balance}")
+       
         
-        
+ 
