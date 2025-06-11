70c03160897769b796b112e703da6576e374382e
Michael Darko <mykdarkoxyz@gmail.com>
2025-06-11 08:02:33 +0000

    created Bank class

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,20 @@
+class BankAccount:
+    def __init__(self,account_balance=0):
+        self.account_balance = account_balance
+        
+    def deposit(self, amount):
+        self.account_balance += amount
+        return self.account_balance
+    
+    def withdraw(self, amount):
+        if self.account_balance >= amount:
+            self.account_balance -= amount
+            return self.account_balance
+        else:
+            return False
+    def display_balance(self):
+        print(f"Current balance: {self.account_balance}")    
+        return self.account_balance
+      
+        
+        
