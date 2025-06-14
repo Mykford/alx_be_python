5a37cda4cf8bfcc04362fbced741fa78699d9073
Michael Darko <mykdarkoxyz@gmail.com>
2025-06-14 10:32:58 +0000

    method names changed

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -16,12 +16,12 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.subtract(5, 3), 2)
         self.assertEqual(self.calc.subtract(0, 0), 0)
 
-    def test_multiplication(self):
+    def test_multiply(self):
         """Test the multiplication method."""
         self.assertEqual(self.calc.multiply(2, 3), 6)
         self.assertEqual(self.calc.multiply(-1, 5), -5)
 
-    def test_division(self):
+    def test_divide(self):
         """Test the division method."""
         self.assertEqual(self.calc.divide(6, 3), 2)
         self.assertIsNone(self.calc.divide(5, 0))  # Division by zero should return None
