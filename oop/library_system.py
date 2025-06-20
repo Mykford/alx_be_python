7d9df4c96ba5a629409c8a9caa62cd7d396935ab
Michael Darko <mykdarkoxyz@gmail.com>
2025-06-20 21:33:56 +0000

    restored list

--- oop/library_system.py
+++ oop/library_system.py
@@ -34,7 +34,7 @@ class Library:
         # print("Books in the library:")
         for book in self.books:
             if isinstance(book, EBook):
-                print(f"EBook: {book.title} by {book.author}, Size: {book.file_size}MB")
+                print(f"EBook: {book.title} by {book.author}, File Size: {book.file_size}KB")
             elif isinstance(book, PrintBook):
                 print(f"PrintBook: {book.title} by {book.author}, Pages: {book.page_count}")
             else:
