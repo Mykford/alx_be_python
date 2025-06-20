3094775e1521b63f5ff36d3b24f5a22fbfdef9d0
Michael Darko <mykdarkoxyz@gmail.com>
2025-06-20 21:22:12 +0000

    fixed code

--- oop/library_system.py
+++ oop/library_system.py
@@ -2,17 +2,25 @@ class Book:
     def __init__(self, title,author):
         self.title = title
         self.author = author
+    def __str__(self):
+        return f"{self.title} by {self.author}"
         
         
 class EBook(Book):
     def __init__(self, title,author,file_size):
         super().__init__(title,author)
-        self.file_size = file_size        
+        self.file_size = file_size 
+    def __str__(self):
+        """String representation of the EBook."""
+        return f"EBook: '{self.title}' by {self.author} (File size: {self.file_size}MB)"           
         
 class PrintBook(Book):
     def __init__(self, title,author,page_count):
         super().__init__(title,author)
         self.page_count = page_count
+    def __str__(self):
+        """String representation of the PrintBook."""
+        return f"PrintBook: '{self.title}' by {self.author} (Pages: {self.page_count})"    
 class Library:
     def __init__(self):
         self._books = []
